7474ff3c9f29badc79c67146a525c5febf14cea3
Shine Sudarsanan <shineklbm@gmail.com>
2015-05-24 20:31:25 +0530

    updated the jstree

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,41 +3,28 @@ angular.module("eLearning", []);
 
 //controller declaration
 angular.module("eLearning")
+	.controller('rootCtrl', ['$scope', '$http', rootController])
 	.controller('pageCtrl', ['$scope', '$http', pageController])
-	.controller('readData', ['$scope', '$http', readData])
 	.controller('imageCtrl', ['$scope', '$http', imageController]);
-//controller function defenition
-/*function readData($scope, $http){
-	$scope.vals = $http.get('app/data/en/data.html')
-					.success(function(data){
-						$scope.$parent.content = data;
-					});
-}*/
+
+function rootController($scope, $http){
+	$http.get('app/config/menus.json')
+		.success(function(data){
+			$scope.menus = data;
+			$('#menu').jstree(data);
+			$("#menu").bind("select_node.jstree", function (e, data) {
+			    return data.instance.toggle_node(data.node);
+			});
+		});
+
+	$scope.changeListName = function(page_id) {
+	    alert(page_id);
+	};
+}
+
 
 function pageController($scope, $http){
-	/*$scope.pageinfo = $http.get('app/config/en/data.json')
-					.success(function(data){
-						$scope.$parent.contents = data;
-					});*/
-	$scope.imageinfo = "demo";
-	$scope.tabinfo = "demo";
 }
 
-function readData($scope, $http){
-	$scope.vals = $http.get('app/data/en/data.json')
-					.success(function(data){
-						$scope.$parent.contents = data;
-					});
-
-}
 function imageController($scope, $http){
-	$scope.init = function(datasource){
-		$scope.datasource = datasource;	
-	}
-	$scope.result = $scope.datasource;
-	/*$scope.pathinfo = $http.get('app/data/en/'+ $scope.datasource +'.json')
-					.success(function(data){
-						$scope.$parent.imageinfo = data;
-					});*/
-
 }
