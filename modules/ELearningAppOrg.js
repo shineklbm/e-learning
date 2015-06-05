e2da86fc961868ba0daeb62359322ba1d7dcc5b0
Shine Sudarsanan <shineklbm@gmail.com>
2015-06-05 10:17:55 +0530

    removed unnecessory files

--- modules/ELearningAppOrg.js
+++
@@ -1,8 +0,0 @@
-angular.module('ELearningApp', ['ngRoute'])
-	.config(function ($routeProvider, $locationProvider){
-		$routeProvider.when('/page1', {
-			controller: 'controllers/pageController',
-			templateUrl: 'views/text-with-image-template.html'
-		})
-		$locationProvider.html5Mode(true);
-	});
