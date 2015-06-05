d495d1435fde1c1c6191c96e02b9f2c031983dc5
Shine Sudarsanan <shineklbm@gmail.com>
2015-06-05 10:30:53 +0530

    udpated the html

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function($){
 		$('#vertical-menu-wrapper').toggle('slow');
 	});
 	var content_height = ($(window).height() - $('#header').height() - $('#footer').height());
-	$("#content-wrapper").height(content_height-72);
+	$("#content-wrapper").height(content_height-80);
 
 	 $('audio').mediaelementplayer({
             videoWidth: '100%',
