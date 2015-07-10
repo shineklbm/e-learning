04cf94c466eb41fb587908308914036d897d496c
Shine Sudarsanan <shineklbm@gmail.com>
2015-07-10 18:16:50 +0530

    Updated the title

--- framework/modules/modules.js
+++ framework/modules/modules.js
@@ -395,6 +395,7 @@ function rootController($scope, $http){
                         if($scope.touch_device && $scope.framework_loaded == false){
                             $("#safari-start-overlay").show();                                
                         }else{
+                            alert("testing");
                             $scope.audioPlayer.play();
                         }
                         $scope.framework_loaded = true;
